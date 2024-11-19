723ac61628476a8007df1569643c8ac47c75fc6c
Hugo Bena <benahugo93@gmail.com>
2024-11-19 17:50:01 +0100

    finalisation de la classe DBConnection.java

--- Patron_SQL/src/activeRecord/DBConnection.java
+++ Patron_SQL/src/activeRecord/DBConnection.java
@@ -1,20 +1,58 @@
 package activeRecord;
 
 
-//3.1. En discutant avec votre enseignant, mettez en oeuvre le patron Singleton (couplé à Factory)
-//pour limiter la connexion à une instance. La classe correspondante doit s'appeler DBConnection,
-//doit disposer d'une méthode getConnection() et doit encapsuler dans des attributs les paramètres
-//de connexion. Modifier la méthode main pour utiliser le patron proposé.
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DBConnection {
+    private static Connection connect = null;
+    private String userName;
+    private String password;
+    private String serverName;
+   private String portNumber;
+    private String tableName;
+    private String dbName;
 
-    private DBConnection dbconnection;
 
-    private static DBConnection(){
-
+    private DBConnection(){
+        this.userName = "root";
+        this.password = "";
+        this.serverName = "127.0.0.1";
+        this.portNumber = "3306";
+        this.tableName = "personne";
+        this.dbName = "testpersonne";
+        try {
+            Properties connectionProps = new Properties();
+            connectionProps.put("user", userName);
+            connectionProps.put("password", password);
+            String urlDB = "jdbc:mysql://" + serverName + ":";
+            urlDB += portNumber + "/" + dbName;
+            connect = DriverManager.getConnection(urlDB, connectionProps);
+            System.out.println(urlDB);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
-    public DBConnection getInstance() {
-        return dbconnection;
+    public static Connection getConnection() {
+        if(connect == null){
+            new DBConnection();
+        }
+        return connect;
     }
+
+
+    /**
+     *Après chaque appel de setNomDB, une nouvelle connexion devra être créée à l'appel
+     * de getConnection. Ceci permet de pouvoir choisir la base dans laquelle effectuer les opérations
+     * (base de test ou base de l’application)
+     */
+
+    public void setNomDB(String nomDB){
+        this.dbName = nomDB;
+    }
+
 }
