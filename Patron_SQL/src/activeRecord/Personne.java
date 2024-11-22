0c00b9237c1eca5202edf1cafafbc1bbe13e0992
Hugo Bena <benahugo93@gmail.com>
2024-11-22 13:13:08 +0100

    Ajouts des classes Personne.java et la classe de test pour connection

---
+++ Patron_SQL/src/activeRecord/Personne.java
@@ -0,0 +1,13 @@
+package activeRecord;
+
+public class Personne {
+    private int id;
+    private String nom;
+    private String prenom;
+
+    public Personne(String nom, String prenom) {
+        this.id = -1;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+}
---
+++ Patron_SQL/src/activeRecord/TestConnection.java
@@ -0,0 +1,44 @@
+package activeRecord;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.sql.Connection;
+
+public class TestConnection {
+
+    @Test
+    public void testSingleton() {
+        Connection conn1 = DBConnection.getConnection();
+        Connection conn2 = DBConnection.getConnection();
+
+        // On verifie que les connections ne sont pas null
+        assertNotNull("La première connexion devrait être différent de null", conn1);
+        assertNotNull("La deuxième connexion devrait être différent de null", conn2);
+
+        // Vérifie que c'est la même instance
+        assertSame("Les connexions devraient être la même instance", conn1, conn2);
+
+        // Vérifie le type de la connexion
+        assertTrue("La connexion devrait être de type java.sql.Connection", conn1 instanceof java.sql.Connection);
+
+    }
+
+    @Test
+    public void testChangementBaseDeDonnees() {
+        //premiere connection
+        Connection conn1 = DBConnection.getConnection();
+
+        // On change de base
+        DBConnection.setNomDB("testetudiant");
+
+        //deuxieme connection
+        Connection conn2 = DBConnection.getConnection();
+
+        // Vérifie que les connexions ne sont pas null
+        assertNotNull("La première connexion devrait être différent de null", conn1);
+        assertNotNull("La deuxieme connexion devrait être différent de null", conn2);
+
+        // instances différentes apres changmeent de base
+        assertNotSame("Les connexions doivent être différentes", conn1, conn2);
+    }
+}
